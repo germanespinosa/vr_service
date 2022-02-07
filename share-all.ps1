fcc747ef6e7fc23884dc54f01d839709c65d365b
German Espinosa <germanespinosa@gmail.com>
2022-02-07 11:27:18 -0600

    added script to share all ports

---
+++ share-all.ps1
@@ -0,0 +1,13 @@
+.\wsl-port-forwarding.ps1 4500
+.\wsl-port-forwarding.ps1 4510
+.\wsl-port-forwarding.ps1 4520
+.\wsl-port-forwarding.ps1 4530
+.\wsl-port-forwarding.ps1 4540
+.\wsl-port-forwarding.ps1 4550
+.\wsl-port-forwarding.ps1 4560
+.\wsl-port-forwarding.ps1 4570
+.\wsl-port-forwarding.ps1 4580
+.\wsl-port-forwarding.ps1 4590
+.\wsl-port-forwarding.ps1 4600
+
+
